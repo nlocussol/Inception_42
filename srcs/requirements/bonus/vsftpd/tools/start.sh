e4a4c803ff279dd1d33d4db6c8a39a0805a23391
nlocusso <nlocusso@nlocusso.nlocusso>
2023-05-23 17:35:04 +0200

    maybe ftp done

--- srcs/requirements/bonus/vsftpd/tools/start.sh
+++ srcs/requirements/bonus/vsftpd/tools/start.sh
@@ -1,7 +1,9 @@
 adduser --disabled-password ${FTP_USER}
-
 echo ${FTP_USER}:${FTP_PASSWD} | chpasswd 
 
-echo ${FTP_USER} >  /etc/vsftpd.userlist
+usermod --home /volumes/ ${FTP_USER} 
+chown ${FTP_USER}:${FTP_USER} /volumes/
+
+echo ${FTP_USER} > /etc/vsftpd.userlist
 
 exec vsftpd /etc/vsftpd.conf 
