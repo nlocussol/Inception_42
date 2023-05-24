e0a5126c3bc4df367888ec2243bdac86fbe75987
nlocusso <nlocusso@nlocusso.nlocusso>
2023-05-24 13:34:09 +0200

    sa doit fonctionner mais c pas le cas ff

--- srcs/requirements/bonus/vsftpd/tools/start.sh
+++ srcs/requirements/bonus/vsftpd/tools/start.sh
@@ -1,9 +1,9 @@
-adduser --disabled-password ${FTP_USER}
-echo ${FTP_USER}:${FTP_PASSWD} | chpasswd 
+adduser --disabled-password "${FTP_USER}"
+echo "${FTP_USER}":"${FTP_PASSWD}" | chpasswd
 
-usermod --home /volumes/ ${FTP_USER} 
-chown ${FTP_USER}:${FTP_USER} /volumes/
+usermod --home /volumes/ "${FTP_USER}"
+chown "${FTP_USER}":"${FTP_USER}" /volumes/
 
-echo ${FTP_USER} > /etc/vsftpd.userlist
+echo "${FTP_USER}" > /etc/vsftpd/vsftpd.userlist
 
-exec vsftpd /etc/vsftpd.conf 
+exec vsftpd /etc/vsftpd/vsftpd.conf
--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -6,7 +6,4 @@ SET PASSWORD FOR '${DB_USER}'@'%' = PASSWORD('${DB_PASSWD}');
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;" > /init.sql
 
-sed -i "s/\${DB_PASSWD}/${DB_PASSWD}/" /init.sql
-sed -i "s/\${DB_USER}/${DB_USER}/" /init.sql
-
 exec /usr/bin/mariadbd --no-defaults --user=root --datadir=/var/lib/mysql --init-file=/init.sql
